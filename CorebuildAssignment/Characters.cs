85801cdeec4df51afa10ec07b939746734b1e7ee
Avram Cătălin <49835245+AvramCatalin@users.noreply.github.com>
2019-05-01 20:00:50 +0300

    v0.1

---
+++ CorebuildAssignment/Characters.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace CorebuildAssignment
+{
+    [XmlRoot("characters")]
+    public class Characters
+    {
+        [XmlElement("character")]
+        public List<Character> Character { get; set; }
+    }
+    public class Character
+    {
+        [XmlElement("attack")]
+        public int Attack { get; set; }
+
+        [XmlElement("description")]
+        public string Description { get; set; }
+
+        [XmlElement("health")]
+        public int Health { get; set; }
+
+        [XmlElement("id")]
+        public int Id { get; set; }
+
+        [XmlElement("isVillain")]
+        public bool IsVillain { get; set; }
+
+        [XmlElement("name")]
+        public string Name { get; set; }
+
+    }
+}
---
+++ CorebuildAssignment/Planets.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace CorebuildAssignment
+{
+    [XmlRoot("planets")]
+    public class Planets
+    {
+        [XmlElement("planet")]
+        public List<Planet> Planet { get; set; }
+    }
+    public class Planet
+    {
+        [XmlElement("description")]
+        public string Description { get; set; }
+
+        [XmlElement("id")]
+        public int Id { get; set; }
+
+        [XmlElement("modifiers")]
+        public List<Modifiers> Modifiers { get; set; }
+
+        [XmlElement("name")]
+        public string Name { get; set; }
+    }
+    public class Modifiers
+    {
+        [XmlElement("heroAttackModifier")]
+        public int HeroAttackModifier { get; set; }
+
+        [XmlElement("heroHealthModifier")]
+        public int HeroHealthModifier { get; set; }
+
+        [XmlElement("villainAttackModifier")]
+        public int VillainAttackModifier { get; set; }
+
+        [XmlElement("villainHealthModifier")]
+        public int VillainHealthModifier { get; set; }
+    }
+}
